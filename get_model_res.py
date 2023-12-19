de8e3f4de2e6eecd15a9b21542c5878b372d93a3
Kaktys36 <nikolaeff.alexandr2016@yandex.ru>
2023-12-19 22:54:30 +0300

    Commits and little changes

--- get_model_res.py
+++ get_model_res.py
@@ -2,17 +2,26 @@ from prompt_generation import prompt_generation
 import requests
 import json
 
+
+# Функция для получения результата работы yandexGPT summorization. 
+# На вход получает промпт-запрос и обработанный после транскрибации текст 
+# На выходе отдаёт ответ модели. Далее он булет отправлен пользователю через телеграм-бота
 def get_model_res(prompt, text):
 
+  # Адресс, необходимый для подключения модели
   url = "https://llm.api.cloud.yandex.net/foundationModels/v1/completion"
+  # Необходимые модели данные для запроса. ЗДЕСЬ УКАЗЫВАЕТСЯ ВАШ API 
   headers = {
       "Content-Type": "application/json",
       "Authorization": "Api-Key API-KEY"
   }
+  # Формирование post-запроса. Здесь же передаётся промпт-запрос, сгенерированый prompt_generation
   response = requests.post(url, headers=headers, json=prompt_generation(text))
+  # Получение текст из ответа модели
   response_text = response.text
+  # Преобразование для работы с json
   response_dict = json.loads(response_text)
 
-# Извлечение текста ответа модели
+# Извлечение текста ответа модели из json
   model_response = response_dict["result"]["alternatives"][0]["message"]["text"]
   return model_response
