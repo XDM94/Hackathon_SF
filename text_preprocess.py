2cf45ef464302e0ca7add8e80ad6b3beb3ec3926
Kaktys36 <nikolaeff.alexandr2016@yandex.ru>
2023-12-19 23:41:55 +0300

    Commits and little changes

--- text_preprocess.py
+++ text_preprocess.py
@@ -2,6 +2,7 @@ from nltk.tokenize import (
     sent_tokenize,
     word_tokenize,
 )  # Для токенизации текста для последующей передачи в модель суммаризации
+
 from spellchecker import (
     SpellChecker,
 )  # Для исправления ошибочно транскрибированных слов
@@ -9,16 +10,20 @@ import re, nltk
 
 nltk.download("punkt")
 
-
+# Функция для обработки полученного с помощью транскрибации текста
+# На вход получает транскрибированный текст (результат работы audio_to_text())
 def text_preprocess(text):
+    # Определение объекта из библиотеки SpellChecker, предназначенного для исправления ошибок транскрибации
     spell = SpellChecker(language="ru")
+    # Исправление ошибок транскрибации
     corrected_text = spell.correction(text)
 
-    # Извлечение текста из таймкодов
+    # Извлечение текста без таймкодов
     text_only = re.sub(
         r"\[\d{2}:\d{2}\.\d+\s*-->\s*\d{2}:\d{2}\.\d+\]\s*", "", corrected_text
     )
 
+    # ЧТО ПРОИСХОДИТ НИЖЕ НАДО УБРАТЬ И БЕЗ ЭТОГО ПОТЕСТИТЬ
     # Разделение на предложения
     sentences = sent_tokenize(text_only, language="russian")
 
