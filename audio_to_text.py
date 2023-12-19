187800241b658c65860a5ee7b12f1561923a17e7
Kaktys36 <nikolaeff.alexandr2016@yandex.ru>
2023-12-19 22:23:31 +0300

    Commit and del temp_audio
    Коммиты и удаление аудио после транскрибации.

--- audio_to_text.py
+++ audio_to_text.py
@@ -1,19 +1,22 @@
 import whisper
+import os
 
-
+# Функция для получения модели whisper, по умолчанию small
 def get_model(size="small"):
     model_name = size
     model = whisper.load_model(model_name)
     return model
 
-
+# Функция для преобразования аудио в текст.
+# На вход получает путь к файлу аудио и модель whisper
+# Возвращает список текста
 def audio_to_text(path_to_audio, model):
-    if not model:
-        model = get_model()
+    if not model: # Если нет загруженной модели, то
+        model = get_model() # она получает её из get_model
         print("Модель была загружена.")
-    result = model.transcribe(path_to_audio, language="ru", fp16=False, verbose=True)
-    text = result['text']
+    result = model.transcribe(path_to_audio, language="ru", fp16=False, verbose=True) # Транскрибация текста из аудио с русским языком, запускается на процессоре
+    text = result['text'] # Получение текста из словаря (вывод model.transcribe)
     
     print("Видео транскрибировано.")
-    # os.remove(path_to_audio)
+    os.remove(path_to_audio) # Удаление файла после его транскрибации
     return text
