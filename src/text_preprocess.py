34fa7375717585a1cdd4649ec84c8fae41f84296
Kaktys36 <nikolaeff.alexandr2016@yandex.ru>
2023-12-20 20:58:15 +0300

    Changes

--- src/text_preprocess.py
+++ src/text_preprocess.py
@@ -1,14 +1,8 @@
-#from nltk.tokenize import (
-#    sent_tokenize,
-#    word_tokenize,
-#)  # Для токенизации текста для последующей передачи в модель суммаризации
-
 from spellchecker import (
     SpellChecker,
 )  # Для исправления ошибочно транскрибированных слов
-import re, nltk
 
-nltk.download("punkt")
+import re 
 
 # Функция для обработки полученного с помощью транскрибации текста
 # На вход получает транскрибированный текст (результат работы audio_to_text())
@@ -22,17 +16,5 @@ def text_preprocess(text):
     text_only = re.sub(
         r"\[\d{2}:\d{2}\.\d+\s*-->\s*\d{2}:\d{2}\.\d+\]\s*", "", corrected_text
     )
-
-    # Разделение на предложения
-    # sentences = sent_tokenize(text_only, language="russian")
-
-    # Токенизация и удаление пунктуации
-    # tokens = []
-    # for sentence in sentences:
-    #    words = word_tokenize(sentence.lower(), language="russian")
-    #    words = [
-    #        word for word in words if word.isalnum()
-    #    ]  # Оставляем только буквенно-цифровые токены
-    #    tokens.append(words)
-    # return tokens
+    
     return text_only
